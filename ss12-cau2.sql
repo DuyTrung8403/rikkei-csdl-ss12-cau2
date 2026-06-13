998b9e1991b100e2af318c1ed3315726a4774735
DuyTrung8403 <124582167+DuyTrung8403@users.noreply.github.com>
2026-06-13 22:27:02 +0700

    Add files via upload

---
+++ ss12-cau2.sql
@@ -0,0 +1,39 @@
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY ,
+    name VARCHAR(100),
+    stock INT
+);
+
+CREATE TABLE sales (
+    sale_id SERIAL PRIMARY KEY ,
+    product_id INT REFERENCES products(product_id),
+    quantity INT
+);
+
+CREATE OR REPLACE FUNCTION check_product_stock()
+    RETURNS TRIGGER AS $$
+DECLARE
+    v_stock INT;
+BEGIN
+    SELECT stock INTO v_stock
+    FROM products
+    WHERE product_id = NEW.product_id;
+
+    IF v_stock IS NULL THEN
+        RAISE EXCEPTION 'Khong ton tai sp id %';
+    ELSIF v_stock < NEW.quantity THEN
+        RAISE EXCEPTION 'Khong du san pham trong kho';
+    END IF;
+
+    UPDATE products
+    SET stock = stock - NEW.quantity
+    WHERE product_id = NEW.product_id;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_before_sales_insert
+    BEFORE INSERT ON sales
+    FOR EACH ROW
+EXECUTE FUNCTION check_product_stock();
